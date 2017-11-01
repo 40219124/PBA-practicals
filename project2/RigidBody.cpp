b48f329af1547ca561429d15b76c221173c40b1a
Tim-Surface <40219124@live.napier.ac.uk>
2017-11-01 20:06:34 +0000

    Calculations/functions relating to the inverse inertial tensor.

--- project2/RigidBody.cpp
+++ project2/RigidBody.cpp
@@ -3,3 +3,18 @@
 RigidBody::RigidBody() {}
 
 RigidBody::~RigidBody() {}
+
+void RigidBody::scale(glm::vec3 scale) {
+	for (int i = 0; i < 3; ++i) {
+		m_dimensions[i] *= scale[i];
+	}
+	m_invInertia = glm::inverse(calcIT());
+	Body::scale(scale);
+}
+
+glm::mat3 RigidBody::calcIT() {
+	glm::mat3 it = glm::mat3(0.0f);
+	it[0][0] = getMass() * (powf(m_dimensions[1], 2.0f) + powf(m_dimensions[2], 2.0f)) / 12.0f;
+	it[1][1] = getMass() * (powf(m_dimensions[0], 2.0f) + powf(m_dimensions[2], 2.0f)) / 12.0f;
+	it[2][2] = getMass() * (powf(m_dimensions[0], 2.0f) + powf(m_dimensions[1], 2.0f)) / 12.0f;
+}
--- project2/RigidBody.h
+++ project2/RigidBody.h
@@ -16,9 +16,12 @@ public:
 	glm::vec3 getAngAcc() { return m_angAcc; }
 	glm::mat3 getInvInertia() { return m_invInertia; }
 	void scale(glm::vec3 vect) {}
-
+	
 private:
+	glm::mat3 calcIT() {}
+
 	float m_density;
+	glm::vec3 m_dimensions = glm::vec3(2.0f, 2.0f, 2.0f);
 	glm::mat3 m_invInertia; // Inverse Inertia
 	glm::vec3 m_angVel; // angular velocity
 	glm::vec3 m_angAcc; // angular acceleration
